414358cd55d827d9ddaa47b7fae1bce6e4a4d12d
vadimdoga <vadim.doga1@gmail.com>
2019-12-15 14:25:18 +0200

    add new route(modify store data)

--- index.js
+++ index.js
@@ -103,6 +103,50 @@ app.post("/stores", (req, res) => {
   }
 });
 
+app.put('/stores/:id', (req, res) => {
+  if(req.session.id){
+    if (
+      req.body.storeName ||
+      req.body.storeEmail ||
+      req.body.storeAddress ||
+      req.body.storeImage
+    ) {
+      Store.findOne({_id: req.params.id})
+        .then(store => {
+          if(
+            req.body.storeName !== store.storeName && 
+            req.body.storeEmail !== store.storeName &&
+            req.body.storeAddress !== store.storeAddress && 
+            req.body.storeImage !== store.storeImage
+          ){
+            req.body.storeName ? (store.storeName = req.body.storeName) : null;
+            req.body.storeEmail ? (store.storeEmail = req.body.storeEmail) : null;
+            req.body.storeAddress ? (store.storeAddress = req.body.storeAddress) : null;
+            req.body.storeImage ? (store.storeImage = req.body.storeImage) : null;
+            console.log(store)
+            store.save()
+              .then(() => {
+                res.json("Store updated");
+              })
+              .catch(err => {
+                console.log("Error saving to db", err)
+              })
+          } else {
+            console.log("You need to introduce new data not the old one.")
+            res.sendStatus(406)
+          }
+        })
+        .catch(err => {
+          console.log("Such store does not exist!", err)
+          res.sendStatus(404)
+        })
+    }
+  } else {
+    console.log("User is not connected.");
+    res.sendStatus(401);
+  }
+});
+
 app.post("/auth/register", (req, res) => {
   if (req.body.email && req.body.username && req.body.password) {
     if(verifyPasswordStrength(req.body.password)){
@@ -197,7 +241,10 @@ app.put("/profile/edit", (req, res) => {
               user.save()
                 .then(() => {
                   res.json("Profile updated");
-                });
+                })
+                .catch(err => {
+                  console.log("Error saving to db", err)
+                })
             } else {
               console.log("You need to introduce new data not the old one.")
               res.sendStatus(406)
@@ -302,8 +349,10 @@ app.delete('/products/:id', (req, res) => {
           console.log("Could not find store")
           res.sendStatus(404)
         })
-      
     })
+  } else {
+    console.log("User is not connected.");
+    res.sendStatus(401);
   }
 });
 
